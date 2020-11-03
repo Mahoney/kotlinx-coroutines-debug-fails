de42b5c4c539664ffe5abffd2f20d25ec6cba5d5
Robert Elliot <rob@lidalia.org.uk>
2020-11-03 11:53:39 +0000

    Trying to reproduce error with debugging coroutines

---
+++ src/test/java/org/example/DebugMeTest.kt
@@ -0,0 +1,12 @@
+package org.example
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class DebugMeTest {
+
+    @Test
+    fun `run me using debug and watch it fail`() {
+        assertTrue(true)
+    }
+}
